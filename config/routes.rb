9073acde8c867f2e88f7b0504d389f201952bb09
harusnoopy <117557776+harusnoopy@users.noreply.github.com>
2023-03-17 08:41:00 +0000

    add item's edit page

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :sessioins, only: [:new, :create, :destroy]
-    get '' => 'admin/homes#top'
+    get '' => 'homes#top'
 
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
 
